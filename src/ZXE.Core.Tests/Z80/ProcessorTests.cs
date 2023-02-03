8bdb0d86c04cb8b3385bee7bc0ddc9755522ee20
Stevö <stevehuwjohn@gmail.com>
2023-02-03 22:26:59 +0000

    WIP

--- src/ZXE.Core.Tests/Z80/ProcessorTests.cs
+++ src/ZXE.Core.Tests/Z80/ProcessorTests.cs
@@ -23,6 +23,28 @@ public class ProcessorTests
     }
 
     [Fact]
+    public void Passes_correct_number_of_bytes_to_instruction()
+    {
+        _ram[65_533] = 0x01;
+
+        var state = new State
+                    {
+                        ProgramCounter = 65_533
+                    };
+
+        _processor.ProcessInstruction(_ram, state);
+
+        _ram[65_534] = 0x01;
+
+        state = new State
+                    {
+                        ProgramCounter = 65_534
+                    };
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => _processor.ProcessInstruction(_ram, state));
+    }
+
+    [Fact]
     public void LD_BC_nn()
     {
         _ram[0] = 0x01;
