6e9ccbdaecfca5affe43323312a5664b1b2dee47
Stevö <stevehuwjohn@gmail.com>
2023-02-06 22:07:45 +0000

    WIP

--- src/ZXE.Core.ThirdPartyTests/Infrastructure/TestRunner.cs
+++ src/ZXE.Core.ThirdPartyTests/Infrastructure/TestRunner.cs
@@ -173,7 +173,19 @@ public class TestRunner
         var result = ExecuteTest(test, tracer);
 
         FormattedConsole.WriteLine("\n&Cyan;        Expected    Actual");
+
         FormattedConsole.WriteLine($"    &Cyan;PC&White;: &Green;0x{test.Final.PC:X4}      {(test.Final.PC == result.State.ProgramCounter ? "&Green;" : "&Red;")}0x{result.State.ProgramCounter:X4}");
+        FormattedConsole.WriteLine($"    &Cyan;SP&White;: &Green;0x{test.Final.SP:X4}      {(test.Final.SP == result.State.StackPointer ? "&Green;" : "&Red;")}0x{result.State.StackPointer:X4}");
+
+        FormattedConsole.WriteLine($"    &Cyan;A &White;: &Green;0x{test.Final.A:X2}        {(test.Final.A == result.State.Registers[Register.A] ? "&Green;" : "&Red;")}0x{result.State.Registers[Register.A]:X2}");
+        FormattedConsole.WriteLine($"    &Cyan;B &White;: &Green;0x{test.Final.B:X2}        {(test.Final.B == result.State.Registers[Register.B] ? "&Green;" : "&Red;")}0x{result.State.Registers[Register.B]:X2}");
+        FormattedConsole.WriteLine($"    &Cyan;C &White;: &Green;0x{test.Final.C:X2}        {(test.Final.C == result.State.Registers[Register.C] ? "&Green;" : "&Red;")}0x{result.State.Registers[Register.C]:X2}");
+        FormattedConsole.WriteLine($"    &Cyan;D &White;: &Green;0x{test.Final.D:X2}        {(test.Final.D == result.State.Registers[Register.D] ? "&Green;" : "&Red;")}0x{result.State.Registers[Register.D]:X2}");
+        FormattedConsole.WriteLine($"    &Cyan;E &White;: &Green;0x{test.Final.E:X2}        {(test.Final.E == result.State.Registers[Register.E] ? "&Green;" : "&Red;")}0x{result.State.Registers[Register.E]:X2}");
+        FormattedConsole.WriteLine($"    &Cyan;H &White;: &Green;0x{test.Final.H:X2}        {(test.Final.H == result.State.Registers[Register.H] ? "&Green;" : "&Red;")}0x{result.State.Registers[Register.H]:X2}");
+        FormattedConsole.WriteLine($"    &Cyan;L &White;: &Green;0x{test.Final.L:X2}        {(test.Final.L == result.State.Registers[Register.L] ? "&Green;" : "&Red;")}0x{result.State.Registers[Register.L]:X2}");
+
+        FormattedConsole.WriteLine(string.Empty);
 
         var trace = tracer.GetTrace();
 
