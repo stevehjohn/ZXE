fa1b312b101c5e18d2b82fea40192b761c8ce03a
Stevö <stevehuwjohn@gmail.com>
2023-02-10 00:09:56 +0000

    WIP

---
+++ src/ZXE.Utilities/Entrypoint.cs
@@ -0,0 +1,15 @@
+﻿using ZXE.Utilities.Utilities;
+
+namespace ZXE.Utilities
+{
+    public static class Entrypoint
+    {
+        //public static void Main(string[] args)
+        public static void Main()
+        {
+            var disassembler = new Disassembler();
+
+            disassembler.LoadData(File.ReadAllBytes("..\\..\\..\\..\\..\\ROM Images\\ZX Spectrum 48K\\image-0.rom"));
+        }
+    }
+}
---
+++ src/ZXE.Utilities/Utilities/Disassembler.cs
@@ -0,0 +1,11 @@
+﻿namespace ZXE.Utilities.Utilities;
+
+public class Disassembler
+{
+    private byte[] _data;
+
+    public void LoadData(byte[] data)
+    {
+        _data = data;
+    }
+}
