d06a9952e5e4ebef5ca5bd77e12d8be7fe229dd3
Stevö <stevehuwjohn@gmail.com>
2023-02-12 21:02:59 +0000

    WIP

---
+++ src/ZXE.Emulator.Host/Display/Monitor.cs
@@ -0,0 +1,51 @@
+﻿namespace ZXE.Emulator.Host.Display;
+
+public class Monitor : Game
+{
+    GraphicsDeviceManager graphics;
+    SpriteBatch spriteBatch;
+
+    public Monitor()
+    {
+        graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void UnloadContent()
+    {
+        // TODO: Unload any non ContentManager content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ src/ZXE.Emulator.Host/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ZXE.Emulator.Host
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (var game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
