51c5d6c1be990d3805ffac27bd6cc2d54cb7506b
Stevö <stevehuwjohn@gmail.com>
2023-02-13 15:54:32 +0000

    WIP

---
+++ src/ZXE.Core.Tests/Z80/ProcessorBitOperationInstructionsTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using ZXE.Core.Infrastructure;
+using ZXE.Core.System;
+using ZXE.Core.Z80;
+
+// ReSharper disable CommentTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable IdentifierTypo
+
+namespace ZXE.Core.Tests.Z80;
+
+public class ProcessorBitOperationInstructionsTests
+{
+    private readonly Processor _processor;
+
+    private readonly Ram _ram;
+
+    private readonly State _state;
+
+    private readonly Ports _ports;
+
+    public ProcessorBitOperationInstructionsTests()
+    {
+        _processor = new Processor();
+
+        _ram = new Ram(Model.Spectrum48K);
+
+        _state = new State();
+
+        _processor.SetState(_state);
+
+        _ports = new Ports();
+    }
+
+    [Fact]
+    public void BIT_b_R()
+    {
+        // BIT 0, B
+        _ram[0] = 0xCB;
+        _ram[1] = 0x40;
+
+        _state.Registers[Register.B] = 0b11111111;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.False(_state.Flags.Zero);
+
+        _state.ProgramCounter = 0;
+
+        _state.Registers[Register.B] = 0b11111110;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.True(_state.Flags.Zero);
+    }
+}
