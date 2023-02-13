690e7e214d0e62e4d67472caa7c5658d4ad3be8b
Stevö <stevehuwjohn@gmail.com>
2023-02-13 15:34:19 +0000

    WIP

---
+++ src/ZXE.Core.Tests/Z80/ProcessorLoadInstructionsTests.cs
@@ -0,0 +1,193 @@
+﻿using Xunit;
+using ZXE.Core.Infrastructure;
+using ZXE.Core.System;
+using ZXE.Core.Z80;
+
+// ReSharper disable CommentTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable IdentifierTypo
+
+namespace ZXE.Core.Tests.Z80;
+
+public class ProcessorLoadInstructionsTests
+{
+    private readonly Processor _processor;
+
+    private readonly Ram _ram;
+
+    private readonly State _state;
+
+    private readonly Ports _ports;
+
+    public ProcessorLoadInstructionsTests()
+    {
+        _processor = new Processor();
+
+        _ram = new Ram(Model.Spectrum48K);
+
+        _state = new State();
+
+        _processor.SetState(_state);
+
+        _ports = new Ports();
+    }
+
+    [Fact]
+    public void LD_RR_nn()
+    {
+        // LD BC, 0x1234
+        _ram[0] = 0x01;
+        _ram[1] = 0x34;
+        _ram[2] = 0x12;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x12, _state.Registers[Register.B]);
+        Assert.Equal(0x34, _state.Registers[Register.C]);
+    }
+
+    [Fact]
+    public void LD_addr_RR_A()
+    {
+        // LD (BC), A
+        _ram[0] = 0x02;
+
+        _state.Registers[Register.B] = 0x12;
+        _state.Registers[Register.C] = 0x34;
+
+        _state.Registers[Register.A] = 0x56;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x56, _ram[0x1234]);
+    }
+
+    [Fact]
+    public void LD_R_n()
+    {
+        // LD B, 0x23
+        _ram[0] = 0x06;
+        _ram[1] = 0x23;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x23, _state.Registers[Register.B]);
+    }
+
+    [Fact]
+    public void LD_addr_nn_R()
+    {
+        // LD (0x1234), A
+        _ram[0] = 0x32;
+        _ram[1] = 0x34;
+        _ram[2] = 0x12;
+
+        _state.Registers[Register.A] = 0x56;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x56, _ram[0x1234]);
+    }
+
+    [Fact]
+    public void LD_R_addr_nn()
+    {
+        // LD A, (0x1234)
+        _ram[0] = 0x3A;
+        _ram[1] = 0x34;
+        _ram[2] = 0x12;
+
+        _ram[0x1234] = 0x56;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x56, _state.Registers[Register.A]);
+    }
+
+    [Fact]
+    public void LD_R_addr_RR()
+    {
+        // LD A, (BC)
+        _ram[0] = 0x0A;
+        _ram[0x1234] = 0x56;
+
+        _state.Registers.WritePair(Register.BC, 0x1234);
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x56, _state.Registers[Register.A]);
+    }
+
+    [Fact]
+    public void LD_addr_nn_RR()
+    {
+        // LD (0x1234), HL
+        _ram[0] = 0x22;
+        _ram[1] = 0x34;
+        _ram[2] = 0x12;
+
+        _state.Registers.WritePair(Register.HL, 0x5678);
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x78, _ram[0x1234]);
+        Assert.Equal(0x56, _ram[0x1235]);
+    }
+
+    [Fact]
+    public void LD_RR_addr_nn()
+    {
+        // LD HL, (0x1234)
+        _ram[0] = 0x2A;
+        _ram[1] = 0x34;
+        _ram[2] = 0x12;
+
+        _ram[0x1234] = 0x56;
+        _ram[0x1235] = 0x78;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x56, _state.Registers[Register.L]);
+        Assert.Equal(0x78, _state.Registers[Register.H]);
+    }
+
+    [Fact]
+    public void LD_SP_nn()
+    {
+        // LD SP, 0x1234
+        _ram[0] = 0x31;
+        _ram[1] = 0x34;
+        _ram[2] = 0x12;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x1234, _state.StackPointer);
+    }
+
+    [Fact]
+    public void LD_addr_RR_n()
+    {
+        // LD (HL), 0xAB
+        _ram[0] = 0x36;
+        _ram[1] = 0xAB;
+
+        _state.Registers.WritePair(Register.HL, 0x1234);
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0xAB, _ram[0x1234]);
+    }
+
+    [Fact]
+    public void LD_R_R()
+    {
+        // LD B, C
+        _ram[0] = 0x41;
+
+        _state.Registers[Register.C] = 0x12;
+
+        _processor.ProcessInstruction(_ram, _ports);
+
+        Assert.Equal(0x12, _state.Registers[Register.B]);
+    }
+}
--- src/ZXE.Core.Tests/Z80/ProcessorTests.cs
+++ src/ZXE.Core.Tests/Z80/ProcessorTests.cs
@@ -60,36 +60,6 @@ public class ProcessorTests
     }
 
     [Fact]
-    public void LD_RR_nn()
-    {
-        // LD BC, 0x1234
-        _ram[0] = 0x01;
-        _ram[1] = 0x34;
-        _ram[2] = 0x12;
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x12, _state.Registers[Register.B]);
-        Assert.Equal(0x34, _state.Registers[Register.C]);
-    }
-
-    [Fact]
-    public void LD_addr_RR_A()
-    {
-        // LD (BC), A
-        _ram[0] = 0x02;
-
-        _state.Registers[Register.B] = 0x12;
-        _state.Registers[Register.C] = 0x34;
-
-        _state.Registers[Register.A] = 0x56;
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x56, _ram[0x1234]);
-    }
-
-    [Fact]
     public void INC_RR()
     {
         // INC BC
@@ -138,18 +108,6 @@ public class ProcessorTests
     }
 
     [Fact]
-    public void LD_R_n()
-    {
-        // LD B, 0x23
-        _ram[0] = 0x06;
-        _ram[1] = 0x23;
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x23, _state.Registers[Register.B]);
-    }
-
-    [Fact]
     public void RLCA()
     {
         // RLCA
@@ -171,36 +129,6 @@ public class ProcessorTests
     }
 
     [Fact]
-    public void LD_addr_nn_R()
-    {
-        // LD (0x1234), A
-        _ram[0] = 0x32;
-        _ram[1] = 0x34;
-        _ram[2] = 0x12;
-
-        _state.Registers[Register.A] = 0x56;
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x56, _ram[0x1234]);
-    }
-
-    [Fact]
-    public void LD_R_addr_nn()
-    {
-        // LD A, (0x1234)
-        _ram[0] = 0x3A;
-        _ram[1] = 0x34;
-        _ram[2] = 0x12;
-
-        _ram[0x1234] = 0x56;
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x56, _state.Registers[Register.A]);
-    }
-
-    [Fact]
     public void ADD_RR_RR()
     {
         // ADD HL, BC
@@ -212,20 +140,6 @@ public class ProcessorTests
         _processor.ProcessInstruction(_ram, _ports);
 
         Assert.Equal(0x1234, _state.Registers.ReadPair(Register.HL));
-    }
-
-    [Fact]
-    public void LD_R_addr_RR()
-    {
-        // LD A, (BC)
-        _ram[0] = 0x0A;
-        _ram[0x1234] = 0x56;
-
-        _state.Registers.WritePair(Register.BC, 0x1234);
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x56, _state.Registers[Register.A]);
     }
 
     [Fact]
@@ -375,22 +289,6 @@ public class ProcessorTests
     }
 
     [Fact]
-    public void LD_addr_nn_RR()
-    {
-        // LD (0x1234), HL
-        _ram[0] = 0x22;
-        _ram[1] = 0x34;
-        _ram[2] = 0x12;
-
-        _state.Registers.WritePair(Register.HL, 0x5678);
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x78, _ram[0x1234]);
-        Assert.Equal(0x56, _ram[0x1235]);
-    }
-
-    [Fact]
     public void JR_Z_e()
     {
         // JR Z, 0x10
@@ -412,23 +310,6 @@ public class ProcessorTests
     }
 
     [Fact]
-    public void LD_RR_addr_nn()
-    {
-        // LD HL, (0x1234)
-        _ram[0] = 0x2A;
-        _ram[1] = 0x34;
-        _ram[2] = 0x12;
-
-        _ram[0x1234] = 0x56;
-        _ram[0x1235] = 0x78;
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x56, _state.Registers[Register.L]);
-        Assert.Equal(0x78, _state.Registers[Register.H]);
-    }
-    
-    [Fact]
     public void JR_NC_e()
     {
         // JR NC, 0x10
@@ -447,19 +328,6 @@ public class ProcessorTests
         _processor.ProcessInstruction(_ram, _ports);
 
         Assert.Equal(0x12, _state.ProgramCounter);
-    }
-
-    [Fact]
-    public void LD_SP_nn()
-    {
-        // LD SP, 0x1234
-        _ram[0] = 0x31;
-        _ram[1] = 0x34;
-        _ram[2] = 0x12;
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x1234, _state.StackPointer);
     }
 
     [Fact]
@@ -501,20 +369,6 @@ public class ProcessorTests
         _processor.ProcessInstruction(_ram, _ports);
 
         Assert.Equal(0x09, _ram[0x1234]);
-    }
-
-    [Fact]
-    public void LD_addr_RR_n()
-    {
-        // LD (HL), 0xAB
-        _ram[0] = 0x36;
-        _ram[1] = 0xAB;
-
-        _state.Registers.WritePair(Register.HL, 0x1234);
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0xAB, _ram[0x1234]);
     }
         
     [Fact]
@@ -563,19 +417,6 @@ public class ProcessorTests
         _processor.ProcessInstruction(_ram, _ports);
 
         Assert.Equal(0x1234, _state.StackPointer);
-    }
-
-    [Fact]
-    public void LD_R_R()
-    {
-        // LD B, C
-        _ram[0] = 0x41;
-
-        _state.Registers[Register.C] = 0x12;
-
-        _processor.ProcessInstruction(_ram, _ports);
-
-        Assert.Equal(0x12, _state.Registers[Register.B]);
     }
 
     [Fact]
