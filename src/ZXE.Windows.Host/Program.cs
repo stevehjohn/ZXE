cebb3f47bbecb6d0236f16bdaeb8100d60794c3e
Stevö <stevehuwjohn@gmail.com>
2023-02-13 20:43:43 +0000

    WIP

---
+++ src/ZXE.Windows.Host/Program.cs
@@ -0,0 +1,13 @@
+﻿using ZXE.Windows.Host.Display;
+
+namespace ZXE.Windows.Host;
+
+public static class Program
+{
+    public static void Main()
+    {
+        using var monitor = new Monitor();
+
+        monitor.Run();
+    }
+}
