5dd7a27df7d4aa5ad40dd94db127e64d075bb2c7
Stevö <stevehuwjohn@gmail.com>
2023-02-13 23:04:09 +0000

    WIP

--- src/ZXE.Windows.Host/Display/Monitor.cs
+++ src/ZXE.Windows.Host/Display/Monitor.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using ZXE.Core.System;
