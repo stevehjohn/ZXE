4ef7bde4a5c6dd233a7b487ca40e40c46854e63a
Stevö <stevehuwjohn@gmail.com>
2023-02-16 22:02:59 +0000

    WIP

--- src/ZXE.Windows.Host/Infrastructure/KeyboardMapper.cs
+++ src/ZXE.Windows.Host/Infrastructure/KeyboardMapper.cs
@@ -37,13 +37,64 @@ public static class KeyboardMapper
     {
         switch (port)
         {
+            case 0xFEFE:
+                return ScanForFEFEKeys(keys);
+
             case 0xFDFE:
                 return ScanForFDFEKeys(keys);
+
+            case 0xFBFE:
+                return ScanForFBFEKeys(keys);
+
+            case 0xF7FE:
+                return ScanForF7FEKeys(keys);
+
+            case 0xEFFE:
+                return ScanForEFFEKeys(keys);
+
+            case 0xDFFE:
+                return ScanForDFFEKeys(keys);
+
+            case 0xBFFE:
+                return ScanForBFFEKeys(keys);
+
+            case 0x7FFE:
+                return ScanFor7FFEKeys(keys);
 
             default:
                 // TODO: Should probably throw an exception.
                 return 0;
         }
+    }
+
+    private static byte ScanForFEFEKeys(Keys keys)
+    {
+        if (keys == Keys.LeftShift)
+        {
+            return 0b11111110;
+        }
+
+        if (keys == Keys.Z)
+        {
+            return 0b11111101;
+        }
+
+        if (keys == Keys.X)
+        {
+            return 0b11111011;
+        }
+
+        if (keys == Keys.C)
+        {
+            return 0b11110111;
+        }
+
+        if (keys == Keys.V)
+        {
+            return 0b11101111;
+        }
+
+        return 0;
     }
 
     private static byte ScanForFDFEKeys(Keys keys)
@@ -99,6 +150,156 @@ public static class KeyboardMapper
         }
 
         if (keys == Keys.T)
+        {
+            return 0b11101111;
+        }
+
+        return 0;
+    }
+
+    private static byte ScanForF7FEKeys(Keys keys)
+    {
+        if (keys == Keys.D1)
+        {
+            return 0b11111110;
+        }
+
+        if (keys == Keys.D2)
+        {
+            return 0b11111101;
+        }
+
+        if (keys == Keys.D3)
+        {
+            return 0b11111011;
+        }
+
+        if (keys == Keys.D4)
+        {
+            return 0b11110111;
+        }
+
+        if (keys == Keys.D5)
+        {
+            return 0b11101111;
+        }
+
+        return 0;
+    }
+
+    private static byte ScanForEFFEKeys(Keys keys)
+    {
+        if (keys == Keys.D0)
+        {
+            return 0b11111110;
+        }
+
+        if (keys == Keys.D9)
+        {
+            return 0b11111101;
+        }
+
+        if (keys == Keys.D8)
+        {
+            return 0b11111011;
+        }
+
+        if (keys == Keys.D7)
+        {
+            return 0b11110111;
+        }
+
+        if (keys == Keys.D6)
+        {
+            return 0b11101111;
+        }
+
+        return 0;
+    }
+
+    private static byte ScanForDFFEKeys(Keys keys)
+    {
+        if (keys == Keys.P)
+        {
+            return 0b11111110;
+        }
+
+        if (keys == Keys.O)
+        {
+            return 0b11111101;
+        }
+
+        if (keys == Keys.I)
+        {
+            return 0b11111011;
+        }
+
+        if (keys == Keys.U)
+        {
+            return 0b11110111;
+        }
+
+        if (keys == Keys.Y)
+        {
+            return 0b11101111;
+        }
+
+        return 0;
+    }
+
+    private static byte ScanForBFFEKeys(Keys keys)
+    {
+        if (keys == Keys.Enter)
+        {
+            return 0b11111110;
+        }
+
+        if (keys == Keys.L)
+        {
+            return 0b11111101;
+        }
+
+        if (keys == Keys.K)
+        {
+            return 0b11111011;
+        }
+
+        if (keys == Keys.J)
+        {
+            return 0b11110111;
+        }
+
+        if (keys == Keys.H)
+        {
+            return 0b11101111;
+        }
+
+        return 0;
+    }
+
+    private static byte ScanFor7FFEKeys(Keys keys)
+    {
+        if (keys == Keys.Space)
+        {
+            return 0b11111110;
+        }
+
+        if (keys == Keys.LeftAlt)
+        {
+            return 0b11111101;
+        }
+
+        if (keys == Keys.M)
+        {
+            return 0b11111011;
+        }
+
+        if (keys == Keys.N)
+        {
+            return 0b11110111;
+        }
+
+        if (keys == Keys.B)
         {
             return 0b11101111;
         }
