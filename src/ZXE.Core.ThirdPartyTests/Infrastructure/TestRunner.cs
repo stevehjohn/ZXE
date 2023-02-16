fb60fd6577fec0b540b85c3f3ae49b9b2ab370fb
Stevö <stevehuwjohn@gmail.com>
2023-02-16 02:01:18 +0000

    WIP

--- src/ZXE.Core.ThirdPartyTests/Infrastructure/TestRunner.cs
+++ src/ZXE.Core.ThirdPartyTests/Infrastructure/TestRunner.cs
@@ -37,10 +37,10 @@ public class TestRunner
         foreach (var file in files)
         {
             //Skip a bunch of tests
-            if (Path.GetFileName(file).CompareTo("ed b1") < 0)
-            {
-                continue;
-            }
+            //if (Path.GetFileName(file).CompareTo("ed b1") < 0)
+            //{
+            //    continue;
+            //}
 
             // End early
             //if (Path.GetFileName(file).CompareTo("dd 2f") > 0)
