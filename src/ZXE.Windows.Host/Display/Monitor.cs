cdd6704be662d884562a78b0b3cf72ff4ed57dc9
Stevö <stevehuwjohn@gmail.com>
2023-02-22 00:19:35 +0000

    WIP

--- src/ZXE.Windows.Host/Display/Monitor.cs
+++ src/ZXE.Windows.Host/Display/Monitor.cs
@@ -100,7 +100,7 @@ public class Monitor : Game
         {
             var loader = new Z80FileLoader(_motherboard.Processor.State, _motherboard.Ram);
 
-            loader.Load("..\\..\\..\\..\\..\\Game Images\\Dizzy\\image-0.z80");
+            loader.Load("..\\..\\..\\..\\..\\Game Images\\Treasure Island Dizzy\\image-0.z80");
         }
 
         base.Update(gameTime);
