e59d8fb9c6ead2c627c83d1defe34eb2833efeba
Stevö <stevehuwjohn@gmail.com>
2023-02-26 15:00:04 +0000

    WIP

--- src/ZXE.Core.ThirdPartyTests/Infrastructure/TestRunner.cs
+++ src/ZXE.Core.ThirdPartyTests/Infrastructure/TestRunner.cs
@@ -1,4 +1,4 @@
-﻿#define UNATTENDED
+﻿//#define UNATTENDED
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
@@ -40,7 +40,7 @@ public class TestRunner
         foreach (var file in files)
         {
             // Skip a bunch of tests
-            //if (Path.GetFileName(file).CompareTo("fd 10") < 0)
+            //if (Path.GetFileName(file).CompareTo("d4") < 0)
             //{
             //    continue;
             //}
