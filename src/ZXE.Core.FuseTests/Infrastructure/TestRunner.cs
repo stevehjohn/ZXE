22ed4e75550dfe141dcc3a773ca91440246a4498
Stevö <stevehuwjohn@gmail.com>
2023-02-27 14:57:42 +0000

    WIP

--- src/ZXE.Core.FuseTests/Infrastructure/TestRunner.cs
+++ src/ZXE.Core.FuseTests/Infrastructure/TestRunner.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿#define UNATTENDED
+using System.Diagnostics;
 using ZXE.Common.ConsoleHelpers;
 using ZXE.Common.DebugHelpers;
 using ZXE.Core.FuseTests.Exceptions;
@@ -25,6 +26,8 @@ public static class TestRunner
 
         Console.CursorVisible = false;
 
+        var failedTestNames = new List<string>();
+
         for (var i = 0; i < input.Length; i++)
         {
             if (string.IsNullOrWhiteSpace(input[i]))
@@ -33,9 +36,15 @@ public static class TestRunner
                 {
                     testCount++;
 
-                    if (RunTest(new TestInput(test.ToArray())))
+                    var testInput = new TestInput(test.ToArray());
+
+                    if (RunTest(testInput))
                     {
                         passed++;
+                    }
+                    else
+                    {
+                        failedTestNames.Add(testInput.Name);
                     }
 
                     test.Clear();
@@ -50,6 +59,31 @@ public static class TestRunner
         FormattedConsole.WriteLine($"\n  &Cyan;Testing complete&White;. &Cyan;Elapsed&White;: &Yellow;{stopwatch.Elapsed.Minutes:D2}:{stopwatch.Elapsed.Seconds:D2}.{stopwatch.Elapsed.Milliseconds:D3}\n");
 
         FormattedConsole.WriteLine($"  &Cyan;Executed&White;: &Yellow;{testCount:N0}    &Cyan;Passed&White;: &Green;{passed:N0}   &Cyan;Failed&White;: &Red;{testCount - passed:N0}");
+
+        if (failedTestNames.Count > 0)
+        {
+            FormattedConsole.WriteLine("\n  &Cyan;Press any key to see failed test names...\n");
+
+            Console.ReadKey();
+
+            var first = true;
+
+            foreach (var name in failedTestNames)
+            {
+                if (first)
+                {
+                    FormattedConsole.Write($"&Red;{name}");
+
+                    first = false;
+                }
+                else
+                {
+                    FormattedConsole.Write($"&White;, &Red;{name}");
+                }
+            }
+
+            Console.WriteLine("\n");
+        }
 
         FormattedConsole.WriteLine("&Green;");
 
@@ -115,20 +149,34 @@ public static class TestRunner
 
         FormattedConsole.WriteLine("         &Cyan;Expected         Actual");
 
-        FormattedConsole.WriteLine($"    &Cyan;AF &White;: &Green;0x{expectedResult.ProcessorState.AF:X4}      &Cyan;AF &White;: {(expectedResult.ProcessorState.AF == processor.State.Registers.ReadPair(Register.AF) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.AF):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;BC &White;: &Green;0x{expectedResult.ProcessorState.BC:X4}      &Cyan;BC &White;: {(expectedResult.ProcessorState.BC == processor.State.Registers.ReadPair(Register.BC) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.BC):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;DE &White;: &Green;0x{expectedResult.ProcessorState.DE:X4}      &Cyan;DE &White;: {(expectedResult.ProcessorState.DE == processor.State.Registers.ReadPair(Register.DE) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.DE):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;HL &White;: &Green;0x{expectedResult.ProcessorState.HL:X4}      &Cyan;HL &White;: {(expectedResult.ProcessorState.HL == processor.State.Registers.ReadPair(Register.HL) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.HL):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;AF'&White;: &Green;0x{expectedResult.ProcessorState.AF_:X4}      &Cyan;AF'&White;: {(expectedResult.ProcessorState.AF_ == processor.State.Registers.ReadPair(Register.AF_) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.AF_):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;BC'&White;: &Green;0x{expectedResult.ProcessorState.BC_:X4}      &Cyan;BC'&White;: {(expectedResult.ProcessorState.BC_ == processor.State.Registers.ReadPair(Register.BC_) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.BC_):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;DE'&White;: &Green;0x{expectedResult.ProcessorState.DE_:X4}      &Cyan;DE'&White;: {(expectedResult.ProcessorState.DE_ == processor.State.Registers.ReadPair(Register.DE_) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.DE_):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;HL'&White;: &Green;0x{expectedResult.ProcessorState.HL_:X4}      &Cyan;HL'&White;: {(expectedResult.ProcessorState.HL_ == processor.State.Registers.ReadPair(Register.HL_) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.HL_):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;IX &White;: &Green;0x{expectedResult.ProcessorState.IX:X4}      &Cyan;IX &White;: {(expectedResult.ProcessorState.IX == processor.State.Registers.ReadPair(Register.IX) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.AF_):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;IY &White;: &Green;0x{expectedResult.ProcessorState.IY:X4}      &Cyan;IY &White;: {(expectedResult.ProcessorState.IY == processor.State.Registers.ReadPair(Register.IY) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.BC_):X4}");
-        FormattedConsole.WriteLine($"    &Cyan;PC &White;: &Green;0x{expectedResult.ProcessorState.PC:X4}      &Cyan;PC &White;: {(expectedResult.ProcessorState.PC == processor.State.ProgramCounter ? "&Green;" : "&Red;")}0x{processor.State.ProgramCounter:X4}");
-        FormattedConsole.WriteLine($"    &Cyan;SP &White;: &Green;0x{expectedResult.ProcessorState.SP:X4}      &Cyan;SP &White;: {(expectedResult.ProcessorState.SP == processor.State.StackPointer ? "&Green;" : "&Red;")}0x{processor.State.StackPointer:X4}");
-        FormattedConsole.WriteLine($"    &Cyan;I  &White;: &Green;0x{expectedResult.ProcessorState.I:X2}        &Cyan;I  &White;: {(expectedResult.ProcessorState.I == processor.State.Registers[Register.I] ? "&Green;" : "&Red;")}0x{processor.State.Registers[Register.I]:X2}");
-        FormattedConsole.WriteLine($"    &Cyan;R  &White;: &Green;0x{expectedResult.ProcessorState.R:X2}        &Cyan;R  &White;: {(expectedResult.ProcessorState.R == processor.State.Registers[Register.R] ? "&Green;" : "&Red;")}0x{processor.State.Registers[Register.R]:X2}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;AF &White;: &Green;0x{expectedResult.ProcessorState.AF:X4}      &Cyan;AF &White;: {(expectedResult.ProcessorState.AF == processor.State.Registers.ReadPair(Register.AF) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.AF):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;BC &White;: &Green;0x{expectedResult.ProcessorState.BC:X4}      &Cyan;BC &White;: {(expectedResult.ProcessorState.BC == processor.State.Registers.ReadPair(Register.BC) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.BC):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;DE &White;: &Green;0x{expectedResult.ProcessorState.DE:X4}      &Cyan;DE &White;: {(expectedResult.ProcessorState.DE == processor.State.Registers.ReadPair(Register.DE) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.DE):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;HL &White;: &Green;0x{expectedResult.ProcessorState.HL:X4}      &Cyan;HL &White;: {(expectedResult.ProcessorState.HL == processor.State.Registers.ReadPair(Register.HL) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.HL):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;AF'&White;: &Green;0x{expectedResult.ProcessorState.AF_:X4}      &Cyan;AF'&White;: {(expectedResult.ProcessorState.AF_ == processor.State.Registers.ReadPair(Register.AF_) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.AF_):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;BC'&White;: &Green;0x{expectedResult.ProcessorState.BC_:X4}      &Cyan;BC'&White;: {(expectedResult.ProcessorState.BC_ == processor.State.Registers.ReadPair(Register.BC_) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.BC_):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;DE'&White;: &Green;0x{expectedResult.ProcessorState.DE_:X4}      &Cyan;DE'&White;: {(expectedResult.ProcessorState.DE_ == processor.State.Registers.ReadPair(Register.DE_) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.DE_):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;HL'&White;: &Green;0x{expectedResult.ProcessorState.HL_:X4}      &Cyan;HL'&White;: {(expectedResult.ProcessorState.HL_ == processor.State.Registers.ReadPair(Register.HL_) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.HL_):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;IX &White;: &Green;0x{expectedResult.ProcessorState.IX:X4}      &Cyan;IX &White;: {(expectedResult.ProcessorState.IX == processor.State.Registers.ReadPair(Register.IX) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.AF_):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;IY &White;: &Green;0x{expectedResult.ProcessorState.IY:X4}      &Cyan;IY &White;: {(expectedResult.ProcessorState.IY == processor.State.Registers.ReadPair(Register.IY) ? "&Green;" : "&Red;")}0x{processor.State.Registers.ReadPair(Register.BC_):X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;PC &White;: &Green;0x{expectedResult.ProcessorState.PC:X4}      &Cyan;PC &White;: {(expectedResult.ProcessorState.PC == processor.State.ProgramCounter ? "&Green;" : "&Red;")}0x{processor.State.ProgramCounter:X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;SP &White;: &Green;0x{expectedResult.ProcessorState.SP:X4}      &Cyan;SP &White;: {(expectedResult.ProcessorState.SP == processor.State.StackPointer ? "&Green;" : "&Red;")}0x{processor.State.StackPointer:X4}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;I  &White;: &Green;0x{expectedResult.ProcessorState.I:X2}        &Cyan;I  &White;: {(expectedResult.ProcessorState.I == processor.State.Registers[Register.I] ? "&Green;" : "&Red;")}0x{processor.State.Registers[Register.I]:X2}");
+        FormattedConsole.WriteLine(
+            $"    &Cyan;R  &White;: &Green;0x{expectedResult.ProcessorState.R:X2}        &Cyan;R  &White;: {(expectedResult.ProcessorState.R == processor.State.Registers[Register.R] ? "&Green;" : "&Red;")}0x{processor.State.Registers[Register.R]:X2}");
 
         FormattedConsole.WriteLine(string.Empty);
 
@@ -143,6 +191,12 @@ public static class TestRunner
         {
             FormattedConsole.WriteLine($"      {line}");
         }
+
+#if ! UNATTENDED
+        FormattedConsole.WriteLine("\n    &Cyan;Press any key to continue...\n");
+
+        Console.ReadKey();
+#endif
 
         return false;
     }
