7a9746a68a0be4fb697d49657274375bda9ad474
Stevö <stevehuwjohn@gmail.com>
2023-02-28 20:14:46 +0000

    WIP

--- src/ZXE.Windows.Host/Program.cs
+++ src/ZXE.Windows.Host/Program.cs
@@ -10,15 +10,9 @@ public static class Program
     public static void Main()
     {
         var motherboard = new Motherboard(Model.Spectrum48K, new FormattingTracer());
-        //var motherboard = new Motherboard(Model.SpectrumPlus3, null);
+        //var motherboard = new Motherboard(Model.SpectrumPlus3, new FormattingTracer());
 
         motherboard.Reset();
-
-        //var data = File.ReadAllBytes("..\\..\\..\\..\\..\\Game Images\\Treasure Island Dizzy\\load-screen.bin");
-
-        //motherboard.LoadData(data, 0x4000);
-
-        //motherboard.Reset(0x4000);
 
         using var monitor = new Monitor(motherboard);
 
