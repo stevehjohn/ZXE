121d40f3921d30b751f29686ee14af374771c10f
Stevö <stevehuwjohn@gmail.com>
2023-03-01 00:59:55 +0000

    WIP

--- src/ZXE.Core/Z80/ProcessorArithmeticInstructions.cs
+++ src/ZXE.Core/Z80/ProcessorArithmeticInstructions.cs
@@ -764,7 +764,7 @@ public static class ProcessorArithmeticInstructions
             // Flags
             input.State.Flags.Carry = result > 0xFF;
             input.State.Flags.AddSubtract = false;
-            input.State.Flags.ParityOverflow = result > 0xFF;
+            input.State.Flags.ParityOverflow = ((valueD ^ valueS) & 0x80) == 0 && ((valueD ^ result) & 0x80) != 0;
             input.State.Flags.X1 = (result & 0x08) > 0;
             input.State.Flags.HalfCarry = (valueD & 0x0F) + (valueS & 0x0F) > 0xF;
             input.State.Flags.X2 = (result & 0x20) > 0;
@@ -794,7 +794,7 @@ public static class ProcessorArithmeticInstructions
             // Flags
             input.State.Flags.Carry = result > 0xFF;
             input.State.Flags.AddSubtract = false;
-            input.State.Flags.ParityOverflow = result > 0x7F;
+            input.State.Flags.ParityOverflow = ((valueD ^ valueS) & 0x80) == 0 && ((valueD ^ result) & 0x80) != 0;
             input.State.Flags.X1 = (result & 0x08) > 0;
             input.State.Flags.HalfCarry = (valueD & 0x0F) + ((valueS + carry) & 0x0F) > 0xF;
             input.State.Flags.X2 = (result & 0x20) > 0;
@@ -858,7 +858,7 @@ public static class ProcessorArithmeticInstructions
             // Flags
             input.State.Flags.Carry = result > 0xFF;
             input.State.Flags.AddSubtract = false;
-            input.State.Flags.ParityOverflow = result > 0x7F;
+            input.State.Flags.ParityOverflow = ((valueD ^ valueS) & 0x80) == 0 && ((valueD ^ result) & 0x80) != 0;
             input.State.Flags.X1 = (result & 0x08) > 0;
             input.State.Flags.HalfCarry = (valueD & 0x0F) + ((valueS + carry) & 0x0F) > 0xF;
             input.State.Flags.X2 = (result & 0x20) > 0;
