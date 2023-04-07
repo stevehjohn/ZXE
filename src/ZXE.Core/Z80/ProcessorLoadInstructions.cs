de998793e1d0324ea149da1598b5dc398d1827a8
Stevö <stevehuwjohn@gmail.com>
2023-04-07 19:14:08 +0100

    WIP

--- src/ZXE.Core/Z80/ProcessorLoadInstructions.cs
+++ src/ZXE.Core/Z80/ProcessorLoadInstructions.cs
@@ -13,9 +13,14 @@ public static class ProcessorLoadInstructions
 
     public static int LD_addr_RR_R(Input input, Register target, Register source)
     {
-        input.Ram[input.State.Registers.ReadPair(target)] = input.State.Registers[source];
+        unchecked
+        {
+            input.Ram[input.State.Registers.ReadPair(target)] = input.State.Registers[source];
 
-        // Flags unaffected
+            // Flags unaffected
+
+            input.State.MemPtr = (ushort) (((input.State.Registers.ReadPair(target) + 1) & 0xFF) | (input.State.Registers[source] << 8));
+        }
 
         return 0;
     }    
@@ -31,29 +36,42 @@ public static class ProcessorLoadInstructions
 
     public static int LD_addr_nn_R(Input input, Register register)
     {
-        input.Ram[(input.Data[2] << 8) | input.Data[1]] = input.State.Registers[register];
+        unchecked
+        {
+            input.Ram[(input.Data[2] << 8) | input.Data[1]] = input.State.Registers[register];
 
-        // Flags unaffected
+            // Flags unaffected
+
+            input.State.MemPtr = (ushort) ((((input.Data[2] << 8) | input.Data[1] + 1) & 0xFF) | (input.State.Registers[register] << 8));
+        }
 
         return 0;
     }
 
     public static int LD_R_addr_nn(Input input, Register register)
     {
-        input.State.Registers[register] = input.Ram[(input.Data[2] << 8) | input.Data[1]];
+        unchecked
+        {
+            input.State.Registers[register] = input.Ram[(input.Data[2] << 8) | input.Data[1]];
 
-        // Flags unaffected
+            // Flags unaffected
+
+            input.State.MemPtr = (ushort) (input.Ram[(input.Data[2] << 8) | input.Data[1]] + 1);
+        }
 
         return 0;
     }
 
     public static int LD_R_addr_RR(Input input, Register target, Register source)
     {
-        input.State.Registers[target] = input.Ram[input.State.Registers.ReadPair(source)];
+        unchecked
+        {
+            input.State.Registers[target] = input.Ram[input.State.Registers.ReadPair(source)];
 
-        // Flags unaffected
+            // Flags unaffected
 
-        input.State.MemPtr = (ushort) (input.State.Registers.ReadPair(source) + 1);
+            input.State.MemPtr = (ushort) (input.State.Registers.ReadPair(source) + 1);
+        }
 
         return 0;
     }
@@ -70,6 +88,8 @@ public static class ProcessorLoadInstructions
             input.Ram[address + 1] = (byte) ((data & 0xFF00) >> 8);
 
             // Flags unaffected
+
+            input.State.MemPtr = (ushort) ((input.Data[2] << 8 | input.Data[1]) + 1);
         }
 
         return 0;
@@ -84,6 +104,8 @@ public static class ProcessorLoadInstructions
             input.State.Registers.WritePair(register, (ushort) (input.Ram[address + 1] << 8 | input.Ram[address]));
 
             // Flags unaffected
+
+            input.State.MemPtr = (ushort) ((input.Data[2] << 8 | input.Data[1]) + 1);
         }
 
         return 0;
