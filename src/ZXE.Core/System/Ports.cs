3ad16bf351552d21d8b9e89e83925aec87b58890
Stevö <stevehuwjohn@gmail.com>
2023-04-20 05:31:15 +0100

    WIP

--- src/ZXE.Core/System/Ports.cs
+++ src/ZXE.Core/System/Ports.cs
@@ -2,20 +2,22 @@
 
 public class Ports
 {
-    private readonly byte[] _ports;
+    private readonly byte?[] _ports;
 
     public Action<byte, byte>? PagedEvent { set; private get; }
 
     public Ports()
     {
-        _ports = new byte[0x01_0000];
+        _ports = new byte?[0x0001_0000];
+
+        _ports[0x1f] = 0;
 
         ResetKeyboardPorts();
     }
 
     public byte ReadByte(ushort port)
     {
-        var value = _ports[port];
+        var value = _ports[port] ?? 0xFF;
 
         if ((port & 0xFE) == 0xFE)
         {
@@ -23,14 +25,14 @@ public class Ports
 
             var high = (port & 0xFF00) >> 8;
 
-            if ((high & 0b0000_0001) == 0) value &= _ports[0b1111_1110_1111_1110];
-            if ((high & 0b0000_0010) == 0) value &= _ports[0b1111_1101_1111_1110];
-            if ((high & 0b0000_0100) == 0) value &= _ports[0b1111_1011_1111_1110];
-            if ((high & 0b0000_1000) == 0) value &= _ports[0b1111_0111_1111_1110];
-            if ((high & 0b0001_0000) == 0) value &= _ports[0b1110_1111_1111_1110];
-            if ((high & 0b0010_0000) == 0) value &= _ports[0b1101_1111_1111_1110];
-            if ((high & 0b0100_0000) == 0) value &= _ports[0b1011_1111_1111_1110];
-            if ((high & 0b1000_0000) == 0) value &= _ports[0b0111_1111_1111_1110];
+            if ((high & 0b0000_0001) == 0) value &= _ports[0b1111_1110_1111_1110] ?? 0xFF;
+            if ((high & 0b0000_0010) == 0) value &= _ports[0b1111_1101_1111_1110] ?? 0xFF;
+            if ((high & 0b0000_0100) == 0) value &= _ports[0b1111_1011_1111_1110] ?? 0xFF;
+            if ((high & 0b0000_1000) == 0) value &= _ports[0b1111_0111_1111_1110] ?? 0xFF;
+            if ((high & 0b0001_0000) == 0) value &= _ports[0b1110_1111_1111_1110] ?? 0xFF;
+            if ((high & 0b0010_0000) == 0) value &= _ports[0b1101_1111_1111_1110] ?? 0xFF;
+            if ((high & 0b0100_0000) == 0) value &= _ports[0b1011_1111_1111_1110] ?? 0xFF;
+            if ((high & 0b1000_0000) == 0) value &= _ports[0b0111_1111_1111_1110] ?? 0xFF;
         }
 
         return value;
