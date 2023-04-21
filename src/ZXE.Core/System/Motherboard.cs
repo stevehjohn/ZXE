f015d4cf99247650a4fcb02b382332d8747e177a
Stevö <stevehuwjohn@gmail.com>
2023-04-21 19:23:36 +0100

    WIP

--- src/ZXE.Core/System/Motherboard.cs
+++ src/ZXE.Core/System/Motherboard.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 #endif
+using System.Diagnostics;
 using ZXE.Core.Infrastructure;
 using ZXE.Core.Infrastructure.Interfaces;
 using ZXE.Core.System.Interfaces;
