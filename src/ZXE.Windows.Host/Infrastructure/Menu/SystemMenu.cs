937bd1452f6cc1854b4b081670edd4ae31f597b4
Stevö <stevehuwjohn@gmail.com>
2023-04-23 20:24:57 +0100

    WIP

--- src/ZXE.Windows.Host/Infrastructure/Menu/SystemMenu.cs
+++ src/ZXE.Windows.Host/Infrastructure/Menu/SystemMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace ZXE.Windows.Host.Infrastructure.Menu;
 
@@ -10,8 +11,8 @@ public class SystemMenu : MenuBase
         var items = new List<Label>
                     {
                         new(true, "ZXE - Select System", Color.White, 0, 0, null),
-                        new(false, "[1] Spectrum 48K", Color.Yellow, 1, 3, '1', Color.Green),
-                        new(true, "[ESC] Close Menu", Color.FromNonPremultiplied(255, 64, 64, 255), 0, 16, '1')
+                        new(false, "[1] Spectrum 48K", Color.Yellow, 1, 3, Keys.D1, Color.Green),
+                        new(true, "[ESC] Close Menu", Color.FromNonPremultiplied(255, 64, 64, 255), 0, 16, Keys.Escape)
                     };
 
         return items;
