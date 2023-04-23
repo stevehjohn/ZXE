ef24d6214454748af8480bc29df29cf935960c2a
Stevö <stevehuwjohn@gmail.com>
2023-04-23 22:23:54 +0100

    WIP

--- src/ZXE.Windows.Host/Infrastructure/Menu/StateMenu.cs
+++ src/ZXE.Windows.Host/Infrastructure/Menu/StateMenu.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace ZXE.Windows.Host.Infrastructure.Menu;
 
@@ -6,11 +8,23 @@ public class StateMenu : MenuBase
 {
     public override List<Label> GetMenu()
     {
-        throw new System.NotImplementedException();
+        var items = new List<Label>
+                    {
+                        new(0, true, "ZXE - State", Color.White, 0, 0, null),
+                        new(1, false, "[1] Save System State", Color.Yellow, 1, 3, Keys.D1, Color.LightGreen),
+                        new(2, false, "[2] Load System State", Color.Yellow, 1, 5, Keys.D2, Color.LightGreen),
+                        new(99, true, "[ESC] Close Menu", Color.FromNonPremultiplied(255, 64, 64, 255), 0, 16, Keys.Escape, Color.LightGreen)
+                    };
+
+        return items;
     }
 
     public override (MenuResult Result, MenuBase NewMenu, object Arguments) ItemSelected(int id)
     {
-        throw new System.NotImplementedException();
+        switch (id)
+        {
+            default:
+                return (MenuResult.NewMenu, new MainMenu(), null);
+        }
     }
 }
