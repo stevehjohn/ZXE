011f942d6736cc1e55909a4c76318b316ba9c5aa
Stevö <stevehuwjohn@gmail.com>
2023-04-23 18:54:47 +0100

    WIP

--- src/ZXE.Windows.Host/Infrastructure/MenuSystem.cs
+++ src/ZXE.Windows.Host/Infrastructure/MenuSystem.cs
@@ -8,6 +8,8 @@ namespace ZXE.Windows.Host.Infrastructure;
 
 public class MenuSystem
 {
+    private const int ColorDecrement = 20;
+
     private readonly Texture2D _background;
 
     private readonly GraphicsDeviceManager _graphicsDeviceManager;
@@ -46,7 +48,10 @@ public class MenuSystem
 
         DrawWindow(data);
 
-        DrawMenuCharacter(data, 'A', 1, 1);
+        DrawString(data, " !\"#$%&'()*+,-./", 1, 1);
+        DrawString(data, "0123456789:;<]>?", 1, 2);
+        DrawString(data, "@ABCDEFGHIJKLMNO", 1, 3);
+        DrawString(data, "PQRSTUVWXYZ[\\]^_", 1, 4);
 
         var screen = new Texture2D(_graphicsDeviceManager.GraphicsDevice, Constants.ScreenWidthPixels, Constants.ScreenHeightPixels);
 
@@ -72,6 +77,14 @@ public class MenuSystem
         }
     }
 
+    private void DrawString(Color[] data, string text, int x, int y)
+    {
+        for (var i = 0; i < text.Length; i++)
+        {
+            DrawMenuCharacter(data, text[i], x + i, y);
+        }
+    }
+
     private void DrawMenuCharacter(Color[] data, char character, int x, int y)
     {
         var co = _characterMap[character];
@@ -92,13 +105,78 @@ public class MenuSystem
                 data[(3 + y) * 2048 + (x + 4) * 8 + ix + iy * 256] = textColor;
             }
 
-            textColor = Color.FromNonPremultiplied(textColor.R - 20, textColor.G - 20, textColor.B - 20, textColor.A);
+            textColor = Color.FromNonPremultiplied(textColor.R - ColorDecrement, textColor.G - ColorDecrement, textColor.B - ColorDecrement, textColor.A);
         }
     }
 
     private void InitialiseCharacterMap()
     {
+        _characterMap.Add(' ', 0);
+        _characterMap.Add('!', 8);
+        _characterMap.Add('"', 16);
+        _characterMap.Add('#', 24);
+        _characterMap.Add('$', 32);
+        _characterMap.Add('%', 40);
+        _characterMap.Add('&', 48);
+        _characterMap.Add('\'', 56);
+        _characterMap.Add('(', 64);
+        _characterMap.Add(')', 72);
+        _characterMap.Add('*', 80);
+        _characterMap.Add('+', 88);
+        _characterMap.Add(',', 96);
+        _characterMap.Add('-', 104);
+        _characterMap.Add('.', 112);
+        _characterMap.Add('/', 120);
+
+        _characterMap.Add('0', 1024);
+        _characterMap.Add('1', 1032);
+        _characterMap.Add('2', 1040);
+        _characterMap.Add('3', 1048);
+        _characterMap.Add('4', 1056);
+        _characterMap.Add('5', 1064);
+        _characterMap.Add('6', 1072);
+        _characterMap.Add('7', 1080);
+        _characterMap.Add('8', 1088);
+        _characterMap.Add('9', 1096);
+        _characterMap.Add(':', 1104);
+        _characterMap.Add(';', 1112);
+        _characterMap.Add('<', 1120);
+        _characterMap.Add('=', 1128);
+        _characterMap.Add('>', 1136);
+        _characterMap.Add('?', 1144);
+
         _characterMap.Add('@', 2048);
         _characterMap.Add('A', 2056);
+        _characterMap.Add('B', 2064);
+        _characterMap.Add('C', 2072);
+        _characterMap.Add('D', 2080);
+        _characterMap.Add('E', 2088);
+        _characterMap.Add('F', 2096);
+        _characterMap.Add('G', 2104);
+        _characterMap.Add('H', 2112);
+        _characterMap.Add('I', 2120);
+        _characterMap.Add('J', 2128);
+        _characterMap.Add('K', 2136);
+        _characterMap.Add('L', 2144);
+        _characterMap.Add('M', 2152);
+        _characterMap.Add('N', 2160);
+        _characterMap.Add('O', 2168);
+
+        _characterMap.Add('P', 3072);
+        _characterMap.Add('Q', 3080);
+        _characterMap.Add('R', 3088);
+        _characterMap.Add('S', 3096);
+        _characterMap.Add('T', 3104);
+        _characterMap.Add('U', 3112);
+        _characterMap.Add('V', 3120);
+        _characterMap.Add('W', 3128);
+        _characterMap.Add('X', 3136);
+        _characterMap.Add('Y', 3144);
+        _characterMap.Add('Z', 3152);
+        _characterMap.Add('[', 3160);
+        _characterMap.Add('\\', 3168);
+        _characterMap.Add(']', 3176);
+        _characterMap.Add('^', 3184);
+        _characterMap.Add('_', 3192);
     }
 }
