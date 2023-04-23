8cfe4149eb0d5290f8db9eea32902d24d92d4f09
Stevö <stevehuwjohn@gmail.com>
2023-04-23 22:21:30 +0100

    WIP

---
+++ src/ZXE.Windows.Host/Infrastructure/Menu/StateMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ZXE.Windows.Host.Infrastructure.Menu;
+
+public class StateMenu : MenuBase
+{
+    public override List<Label> GetMenu()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override (MenuResult Result, MenuBase NewMenu, object Arguments) ItemSelected(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+}
