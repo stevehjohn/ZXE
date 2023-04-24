e7f22ecd295512d385c2f8dac1fc54637102b4d3
Stevo John <stevehuwjohn@gmail.com>
2023-04-24 13:14:27 +0100

    x-platform

---
+++ src/ZXE.Desktop.Host/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ZXE.Desktop.Host;
+
+public static class Program
+{
+    public static void Main()
+    {
+        using var monitor = new Infrastructure.Host();
+
+        monitor.Run();
+    }
+}
