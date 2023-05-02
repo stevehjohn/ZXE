8c5fec43d2ea75920b64980806e3804ecf0d6e3b
Stevö <stevehuwjohn@gmail.com>
2023-05-02 13:28:19 +0100

    WIP

--- src/ZXE.Core/Z80/Processor.cs
+++ src/ZXE.Core/Z80/Processor.cs
@@ -67,6 +67,10 @@ public partial class Processor
 
             instruction!.Action(new Input(Array.Empty<byte>(), _state, ram, ports));
 
+            _opcodesExecuted++;
+
+            _log.Add($"OE: {OpcodesExecuted:X8}  TS: {0:X2}  PC: {_state.ProgramCounter + (_state.OpcodePrefix > 0xFF ? 2 : 0):X8}  AF: {_state.Registers[Register.A]:X2}{_state.Registers[Register.F] & 0b1101_0111:X2}  BC: {_state.Registers.ReadPair(Register.BC):X4}  DE: {_state.Registers.ReadPair(Register.DE):X4}  HL: {_state.Registers.ReadPair(Register.HL):X4}  OP: {instruction.Opcode:X8}  {instruction.Mnemonic}");
+
             return (instruction.ClockCycles, instruction.Mnemonic);
         }
 
